76f283400349795a0352929700fd8b96c7c1537c
Rushil Ekhande <rushilekhande@gmail.com>
2025-03-02 06:39:12 +0530

    responsive stuff

--- app/(dashboard)/agency-dashboard/[agencyId]/volunteers/[volunteerId]/_components/volunteer-form.tsx
+++ app/(dashboard)/agency-dashboard/[agencyId]/volunteers/[volunteerId]/_components/volunteer-form.tsx
@@ -18,7 +18,7 @@ import { useToast } from "@/hooks/use-toast"
 
 interface VolunteerFormProps {
   initialData: Volunteer
-  agencyId: string  
+  agencyId: string
 }
 
 const formSchema = z.object({
@@ -57,49 +57,49 @@ const VolunteerForm = ({ initialData, agencyId }: VolunteerFormProps) => {
         console.error("Missing required data for volunteer check:", { username, email, agencyId });
         return false;
       }
-  
+
       const volunteersRef = collection(db, `agencies/${agencyId}/volunteers`);
       const userRef = collection(db, "users");
-  
+
       const volunteerQuery = query(
         volunteersRef,
         where("username", "==", username),
         where("email", "==", email)
       );
-  
+
       const userQuery = query(
         userRef,
         where("username", "==", username),
         where("email", "==", email)
       );
-  
+
       const querySnapshot = await getDocs(volunteerQuery);
       const userSnapshot = await getDocs(userQuery);
-  
+
       if (querySnapshot.empty && userSnapshot.empty) {
         return false;
       }
-  
+
       return true;
     } catch (error) {
       console.error("Error checking if volunteer exists:", error);
       return false;
     }
-  };  
+  };
 
   const addVolunteer = async (agencyId: string, volunteerData: any) => {
     try {
-        const volunteersRef = collection(db, `agencies/${agencyId}/volunteers`);
+      const volunteersRef = collection(db, `agencies/${agencyId}/volunteers`);
 
-        const newVolunteer: Volunteer = {...volunteerData, hasSetPermanentPassword: false,  totalPoints: 0, points: 0, exp: 0, lastReportDate: null, lastUpdated: "", level: 1, pickupsToday: 4, rank: "rookie", status: "unavailable", streak: 0, address: "", };
+      const newVolunteer: Volunteer = { ...volunteerData, hasSetPermanentPassword: false, totalPoints: 0, points: 0, exp: 0, lastReportDate: null, lastUpdated: "", level: 1, pickupsToday: 4, rank: "rookie", status: "unavailable", streak: 0, address: "", };
 
-        const volunteerDoc = await addDoc(volunteersRef, newVolunteer);
+      const volunteerDoc = await addDoc(volunteersRef, newVolunteer);
 
-        console.log("Volunteer added successfully!");
-      } catch (error) {
-          console.error("Error adding volunteer:", error);
-      }
-  }; 
+      console.log("Volunteer added successfully!");
+    } catch (error) {
+      console.error("Error adding volunteer:", error);
+    }
+  };
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
@@ -136,11 +136,11 @@ const VolunteerForm = ({ initialData, agencyId }: VolunteerFormProps) => {
       setIsLoading(false);
     }
   }
-  
+
   const fileSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
       setIsLoading(true);
-      data.createdAt = new Date(); 
+      data.createdAt = new Date();
       await addVolunteer(agencyId, data);
 
       toast({
@@ -156,7 +156,7 @@ const VolunteerForm = ({ initialData, agencyId }: VolunteerFormProps) => {
       setIsLoading(false);
     }
   }
-  
+
   const onFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
@@ -167,14 +167,14 @@ const VolunteerForm = ({ initialData, agencyId }: VolunteerFormProps) => {
         setSelectedFile(null);
         return;
       }
-      
+
       const reader = new FileReader();
       reader.onload = (e) => {
         const data = new Uint8Array(e.target?.result as ArrayBuffer);
         const workbook = XLSX.read(data, { type: "array" });
         const sheet = workbook.Sheets[workbook.SheetNames[0]];
         const rows: { username: string; email: string }[] = XLSX.utils.sheet_to_json(sheet);
-        
+
         rows.forEach(async (row) => {
           if (!row.username || !row.email) {
             console.warn(`Row missing required fields: ${JSON.stringify(row)}`);
@@ -183,9 +183,9 @@ const VolunteerForm = ({ initialData, agencyId }: VolunteerFormProps) => {
               description: `Required Fields: ${JSON.stringify(row)}`,
               variant: "destructive"
             })
-            return; 
+            return;
           }
-  
+
           const volunteerExists = await checkVolunteerExists(row.username, row.email, agencyId);
           if (!volunteerExists) {
             await fileSubmit({ username: row.username, email: row.email, agencyId });
@@ -202,95 +202,135 @@ const VolunteerForm = ({ initialData, agencyId }: VolunteerFormProps) => {
       router.push(`/agency-dashboard/${agencyId}/volunteers`);
       router.refresh();
     }
-  };  
+  };
 
   return (
     <>
-
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center space-y-4 p-4">
         <Heading title={title} description={description} />
-        <Button variant="default" onClick={handleToggleMode}>
+        <Button variant="default" onClick={handleToggleMode} className="w-full sm:w-auto">
           {isManualEntry ? "Add via File" : "Add Manually"}
         </Button>
       </div>
-      
+
       <Separator />
 
-      {isManualEntry ? 
-        (
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
-              <div className="grid grid-cols-3 gap-8">
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Username</FormLabel>
-                      <FormControl>
-                        <Input disabled={isLoading} placeholder="Your Volunteer Username..." {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input disabled={isLoading} placeholder="Your Volunteer Email..." {...field} type="email" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="agencyId"
-                  defaultValue={agencyId}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>AgencyId</FormLabel>
-                      <FormControl>
-                        <Input disabled={isLoading} {...field} value={agencyId}/>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-    
-                <FormField
-                  control={form.control}
-                  name="createdAt"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Created At</FormLabel>
-                      <FormControl>
-                        <Input disabled={true} {...field} value={`${newDate.getDate()}-${newDate.getMonth()}-${newDate.getFullYear()}`}/>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-    
-              <Button type="submit" disabled={isLoading} size={"sm"} >
-                {action}
-              </Button>
-            </form>
-          </Form>
-        )
-        : (
-            <div className="space-y-4">
-              <label htmlFor="xlupload">Upload Volunteer Excel File</label>
-              <Input type="file" accept=".xlsx, .xls" name="xlupload" onChange={onFileUpload} />
-              <p className="text-sm text-gray-500">File must include "username" and "email" columns compulsorily.</p>
+      {isManualEntry ? (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8 p-4">
+            {/* Responsive Grid for Form Fields */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+              {/* Username Field */}
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        placeholder="Your Volunteer Username..."
+                        {...field}
+                        className="w-full"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Email Field */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        placeholder="Your Volunteer Email..."
+                        {...field}
+                        type="email"
+                        className="w-full"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Agency ID Field */}
+              <FormField
+                control={form.control}
+                name="agencyId"
+                defaultValue={agencyId}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Agency ID</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        {...field}
+                        value={agencyId}
+                        className="w-full"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Created At Field */}
+              <FormField
+                control={form.control}
+                name="createdAt"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Created At</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={true}
+                        {...field}
+                        value={`${newDate.getDate()}-${newDate.getMonth()}-${newDate.getFullYear()}`}
+                        className="w-full"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-          )
-      }
+
+            {/* Submit Button */}
+            <Button
+              type="submit"
+              disabled={isLoading}
+              size="sm"
+              className="w-full sm:w-auto"
+            >
+              {action}
+            </Button>
+          </form>
+        </Form>
+      ) : (
+        <div className="space-y-4 p-4">
+          <label htmlFor="xlupload" className="block text-sm font-medium">
+            Upload Volunteer Excel File
+          </label>
+          <Input
+            type="file"
+            accept=".xlsx, .xls"
+            name="xlupload"
+            onChange={onFileUpload}
+            className="w-full"
+          />
+          <p className="text-sm text-gray-500">
+            File must include "username" and "email" columns compulsorily.
+          </p>
+        </div>
+      )}
     </>
   )
 }
